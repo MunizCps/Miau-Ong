ee04c7d414413fb71ada7a1f66f175b0e3662d33
MunizCps <123003293+MunizCps@users.noreply.github.com>
2023-01-22 13:22:41 -0300

    Delete projeto Dsw 08122021/Projeto dsw directory

--- projeto Dsw 08122021/Projeto dsw/rodape.php
+++
@@ -1,5 +0,0 @@
-<footer class="layout-footer">
-		   <div class="container">
-			   <span class="footer-copy" style=" color: white;" >&copy; 2021 Bijole. Todos os direitos reservados.</span>
-		    </div>
-	</footer> 
